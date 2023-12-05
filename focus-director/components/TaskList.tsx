91a110f5f9a42c4151f64c358125bf4e7d373ff0
Heming Hanevik <Heming.Hanevik@student.uib.no>
2023-12-05 17:28:38 +0100

    ➕💄Added SASS + added styles to components

--- focus-director/components/TaskList.tsx
+++ focus-director/components/TaskList.tsx
@@ -1,21 +1,59 @@
+"use client";
+
 import TaskCard from './TaskCard'
+import '../styles/styles.scss'
+import { useState } from 'react'
 
 type TaskListProps = {
     tasks: string[]
 }
 
 const TaskList: React.FC<TaskListProps> = ({tasks}) => {
+    const [listed, setListed] = useState<string[]>(tasks)
+    const [description, setDescription] = useState<string>('')
+
+    const submitNewTask = () => {
+        if (description.trim() !== '') {
+            setListed([...listed, description])
+            setDescription('')
+        }
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDescription(event.target.value);
+    }
+
+    const handleRemoveTask = (index: number) => {
+        const updatedList = [...listed]
+        updatedList.splice(index, 1)
+        setListed(updatedList)
+    }
 
     return (
-        <div>
+        <>
+        <div className="taskContainer">
             <ul>
-                {tasks.map((task) => (
-                    <li>
+                {listed.map((task, index) => (
+                    <li key={index}>
                         <TaskCard description={task} />
+                        <button onClick={() => handleRemoveTask(index)}>Remove</button>
                     </li>
                 ))}
+                <div>
+                    <label>Create new step</label>
+                    <input
+                    type="text"
+                    value={description}
+                    onChange={handleChange}
+                    placeholder="Describe step"
+                    >
+                    </input>
+                    <button onClick={submitNewTask}>Add Step</button>
+                </div>
             </ul>
         </div>
+
+        </>
     )
 }
 
