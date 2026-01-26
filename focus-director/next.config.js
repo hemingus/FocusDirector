1c8e9272bf9b675ba449def1d1afe178a4e68257
hemingus <heming.hanevik@gmail.com>
2026-01-26 16:09:01 +0100

    config svgr

--- focus-director/next.config.js
+++ focus-director/next.config.js
@@ -1,4 +1,40 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    // Find the existing SVG rule
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test?.test?.('.svg')
+    );
 
-module.exports = nextConfig
+    // Add our custom SVG handling
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+            },
+          },
+        ],
+      }
+    );
+
+    // Exclude SVGs from the default loader
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
