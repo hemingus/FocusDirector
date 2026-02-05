58af2cbb685954d724067d76ee066dc6f098092d
hemingus <heming.hanevik@gmail.com>
2026-02-05 10:55:54 +0100

    add confirm provider to app - global confirm dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import './globals.scss'
+import { ConfirmProvider } from '@/components/ConfirmDialog/ConfirmContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConfirmProvider>
+          {children}
+        </ConfirmProvider>
+      </body>
     </html>
   )
 }
