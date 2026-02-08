5b54a9307bf59042ca0a0b74fa9aa8d62e2e68f0
Heming Hanevik <heming.hanevik@gmail.com>
2026-02-08 23:58:05 +0100

    add landing page blueprint

---
+++ src/app/LandingPage/page.tsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+    return (
+        <div className="mainPage">
+            <h1>Focus Director</h1>
+            <img src="./assets/focus_logo.png" alt="Focus Director logo" />
+        </div>
+    )
+}
