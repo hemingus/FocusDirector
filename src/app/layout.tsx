19c9fc3708f6c6da4a55b33b9ee6f403cca5f277
Heming Hanevik <heming.hanevik@gmail.com>
2026-02-12 23:54:15 +0100

    rename files + update imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import './globals.scss'
 import { ConfirmProvider } from '@/components/ConfirmDialog/ConfirmContext'
-import Navbar from '@/components/NavBar/NavBar'
+import Navbar from '@/components/Navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
