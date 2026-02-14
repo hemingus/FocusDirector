708c49c023098591d3804c24c32216ae00dad9d0
Heming Hanevik <heming.hanevik@gmail.com>
2026-02-14 23:57:50 +0100

    add paragraph to landing page

--- src/app/LandingPage/page.tsx
+++ src/app/LandingPage/page.tsx
@@ -2,6 +2,10 @@ export default function LandingPage() {
     return (
         <div className="mainPage">
             <h1>Focus Director</h1>
+            <p>Welcome to Focus Director - a smarter way to move work forward.<br/> 
+                Structure your tasks with clarity, break them into manageable steps, and eliminate overwhelm. <br/>
+                With an intelligent Focus Mode that guides you to the next actionable step, <br/>
+                 Focus Director helps you concentrate, make steady progress, and execute with purpose.</p>
             <img src="./assets/focus_logo.png" alt="Focus Director logo" />
         </div>
     )
