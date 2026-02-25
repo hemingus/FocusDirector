1b8012ad6c6cadd86fd9c6fa727d378ddd1362c0
hemingus <heming.hanevik@gmail.com>
2026-02-25 15:23:57 +0100

    change nav link

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
           }`}
         >
           <li><a href="/Testroute">Home</a></li>
-          <li><a href="/LandingPage">Projects</a></li>
+          <li><a href="/Dashboard">Dashboard</a></li>
           <li><a href="/">About</a></li>
 
           <div className={`${styles.navbar__actions} ${styles.mobile}`}>
