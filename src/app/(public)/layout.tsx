ccaa00a4137befedac5a3f01529863897e849794
hemingus <heming.hanevik@gmail.com>
2026-02-26 14:34:22 +0100

    improve base layout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,17 +1,14 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      {/* Optional public navbar */}
-      <header>
-        <h1>Focus Director</h1>
-      </header>
-
+    <div className="mainPage">
+      <Navbar />
       <main>{children}</main>
-
       <footer>© 2026 Focus Director</footer>
     </div>
   );
