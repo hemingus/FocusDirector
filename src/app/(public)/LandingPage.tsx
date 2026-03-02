0d13189005762bdb7fa2c334db660c2c2fad25e6
hemingus <heming.hanevik@gmail.com>
2026-03-02 14:50:51 +0100

    add logo to landing page

--- src/app/(public)/LandingPage.tsx
+++ src/app/(public)/LandingPage.tsx
@@ -5,6 +5,7 @@ export default function LandingPage() {
     return (
         <div className={styles.landing}>
             <h1>Focus Director</h1>
+            <img src="./assets/focus_director_label_outlined.png" alt="focus director" />
             <p>Welcome to Focus Director — a smarter way to move work forward. 
                 Structure your tasks with clarity, break them into manageable steps, and eliminate overwhelm. 
                 With an intelligent Focus Mode that guides you to the next actionable step, 
