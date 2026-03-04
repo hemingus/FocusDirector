4f087fe7be893995e40401a82055e61528078c03
hemingus <heming.hanevik@gmail.com>
2026-03-04 14:40:16 +0100

    update landing page

--- src/app/(public)/LandingPage.tsx
+++ src/app/(public)/LandingPage.tsx
@@ -4,15 +4,20 @@ import styles from './LandingPage.module.scss'
 export default function LandingPage() {
     return (
         <div className={styles.landing}>
-            <h1>Focus Director</h1>
-            <img src="./assets/focus_director_label_outlined.png" alt="focus director" />
-            <p>Welcome to Focus Director — a smarter way to move work forward. 
-                Structure your tasks with clarity, break them into manageable steps, and eliminate overwhelm. 
-                With an intelligent Focus Mode that guides you to the next actionable step, 
-                Focus Director helps you concentrate, make steady progress, and execute with purpose.</p>
-            <div className={styles.landing__cta}>
-                <Link href="/login">Login</Link>
-                <Link href="/register">Register</Link>
+            {/* <h1>Welcome to</h1> */}
+            <img src="./assets/focusdirector_logo.png" alt="focus director" />
+            <div className={styles.landing__text}>
+                <h1>Welcome to Focus Director</h1>
+                <h2> — a smarter way to move work forward. </h2>
+                    <div className={styles.landing__cta}>
+                        <Link href="/login">Login</Link>
+                        <Link href="/register">Register</Link>
+                    </div>
+                    <div className={styles.landing__message}>
+                        <p>Structure your tasks with clarity, break them into manageable steps, and eliminate overwhelm. </p>
+                        <p>With an intelligent Focus Mode that guides you to the next actionable step. </p>
+                        <p>Focus Director helps you concentrate, make steady progress, and execute with purpose.</p>
+                    </div>
             </div>
         </div>
     )
