2837a55de8326d67dcbe741510cafeed39aab1c6
hemingus <heming.hanevik@gmail.com>
2026-03-05 15:07:25 +0100

    remove redundant h1 from dashboard page

--- src/app/(dashboard)/Dashboard/page.tsx
+++ src/app/(dashboard)/Dashboard/page.tsx
@@ -4,7 +4,6 @@ import DashBoard from './DashBoard'
 export default function Home() {
     return (
         <div className="mainPage">
-            <h1>Focus Director</h1>
             <DashBoard />
         </div>
     )
