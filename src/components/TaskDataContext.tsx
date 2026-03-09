86efd1e0bfdc36dc7110fed2c0d8167ad80a520f
hemingus <heming.hanevik@gmail.com>
2026-03-09 15:24:14 +0100

    update task data context with project id

--- src/components/TaskDataContext.tsx
+++ src/components/TaskDataContext.tsx
@@ -6,22 +6,33 @@ import { Task } from './TaskTypes'
 interface TaskDataContextProps {
     taskData: Task[]
     setTaskData: Dispatch<SetStateAction<Task[]>>
-    getTasks: () => Promise<void>
+    getTasks: (projectId: string) => Promise<void>
     loading: boolean
 }
 
 interface TaskDataProviderProps {
     children: ReactNode
+    projectId: string
 }
 
 const TaskDataContext = createContext<TaskDataContextProps | undefined>(undefined);
 
-export const TaskDataProvider: React.FC<TaskDataProviderProps> = ({ children }) => {
+export const TaskDataProvider: React.FC<TaskDataProviderProps> = ({ children, projectId }) => {
     const [taskData, setTaskData] = useState<Task[]>([])
     const [loading, setLoading] = useState(true)
 
-    const getTasks = async (url = "https://hemingmusicapi.azurewebsites.net/TaskEntity") => {
-        await fetch(url, {method: 'GET'})
+    // old url: "https://hemingmusicapi.azurewebsites.net/TaskEntity"
+
+    const getTasks = async (projectId: string) => {
+        const url = `https://localhost:7172/Project/${projectId}/tasks`
+        await fetch(url, 
+            {
+                credentials: "include",
+                method: 'GET',
+                headers:{
+                    "Content-Type": "application/json"
+                }
+            })
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -34,8 +45,10 @@ export const TaskDataProvider: React.FC<TaskDataProviderProps> = ({ children }) 
     }
 
     useEffect(() => {
-        getTasks()
-    }, [])
+        if (projectId) {
+            getTasks(projectId)
+        }   
+    }, [projectId])
 
     const contextValue: TaskDataContextProps = {
         taskData, 
