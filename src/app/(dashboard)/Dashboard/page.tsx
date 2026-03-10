54c176e1b2ed3ceb224f2f1a658ef33f2626575e
hemingus <heming.hanevik@gmail.com>
2026-03-10 12:38:02 +0100

    add DashboardNav to dashboard page

--- src/app/(dashboard)/Dashboard/page.tsx
+++ src/app/(dashboard)/Dashboard/page.tsx
@@ -1,9 +1,11 @@
 import DashBoard from './DashBoard'
+import DashboardNav from './DashboardNav'
 
 
 export default function Home() {
     return (
         <div className="mainPage">
+            <DashboardNav />
             <DashBoard />
         </div>
     )
