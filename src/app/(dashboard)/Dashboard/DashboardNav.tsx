ba15bf4a2b2fd9865bce92d8d450489d0aa3d295
hemingus <heming.hanevik@gmail.com>
2026-03-10 14:49:12 +0100

    implement base dashboard nav component

--- src/app/(dashboard)/Dashboard/DashboardNav.tsx
+++ src/app/(dashboard)/Dashboard/DashboardNav.tsx
@@ -1,9 +1,51 @@
-import Link from "next/link";
+"use client";
 
-export default function DashboardNav() {
+import { useAuth } from "@/context/AuthContext";
+import styles from "../../../components/Navbar/Navbar.module.scss";
+
+const DashboardNav = () => {
+  const { user, loading, logout } = useAuth();
+
+  if (loading) {
     return (
-        <nav>
-            <Link href="/">Home</Link>
-        </nav>
-    )
-}
+      <nav className={styles.navbar}>
+        <div className={styles.navbar__container}>
+          <p>Loading user...</p>
+        </div>
+      </nav>
+    );
+  }
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar__container}>
+        <div className={styles.navbar__logo}>
+          <h2>Dashboard</h2>
+        </div>
+
+        <div className={styles.navbar__actions}>
+          {user ? (
+            <>
+              <div>
+                <span>{user.name}</span>
+                <br />
+                <span>{user.email}</span>
+              </div>
+
+              <button
+                className={`${styles.btn} ${styles.btnOutline}`}
+                onClick={logout}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <p>Not logged in</p>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default DashboardNav;
