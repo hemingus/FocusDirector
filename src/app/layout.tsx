713ae4cc6275dca483fac5fe604a52cc2792b569
hemingus <heming.hanevik@gmail.com>
2026-03-10 14:49:42 +0100

    add auth provider to root app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import './globals.scss'
 import { ConfirmProvider } from '@/components/ConfirmDialog/ConfirmContext'
+import { AuthProvider } from '@/context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConfirmProvider>
-          {children}
-        </ConfirmProvider>
+        <AuthProvider>
+          <ConfirmProvider>
+            {children}
+          </ConfirmProvider>
+        </AuthProvider>
       </body>
     </html>
   )
