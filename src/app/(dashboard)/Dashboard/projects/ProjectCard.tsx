9efbcbde9b153d93910f183bfa55e029fdc29b3b
Heming Hanevik <heming.hanevik@gmail.com>
2026-03-20 08:42:44 +0100

    add delete button on project card

--- src/app/(dashboard)/Dashboard/projects/ProjectCard.tsx
+++ src/app/(dashboard)/Dashboard/projects/ProjectCard.tsx
@@ -1,12 +1,19 @@
+"use client"
+
 import { Project } from "@/context/ProjectContext"
 import styles from './Projects.module.scss'
 
 export default function ProjectCard({id, name, description, createdAt}:Project) {
+    function handleDelete() {
+        console.log("project removed.")
+    }
+
     return (
         <div className={styles.project__card} key={id}>
             <h3>{name}</h3>
             <p>{description}</p>
             <h6>{createdAt}</h6>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     )
 }
