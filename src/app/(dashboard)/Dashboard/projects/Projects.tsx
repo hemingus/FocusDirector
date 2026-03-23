f34f2c3247682db7101a8e1f722ccbd69608c7c6
Heming Hanevik <heming.hanevik@gmail.com>
2026-03-23 23:59:50 +0100

    update projects

--- src/app/(dashboard)/Dashboard/projects/Projects.tsx
+++ src/app/(dashboard)/Dashboard/projects/Projects.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { useProjects } from "@/context/ProjectContext";
 import styles from './Projects.module.scss'
 import ProjectCard from "./ProjectCard";
+import { useState } from "react";
+import AddNewProject from "./AddNewProject";
 
 export default function Projects() {
   const { projects, loading, error } = useProjects();
+  const [showAddForm, setShowAddForm] = useState(false);
 
   if (loading) {
     return <p>Loading projects...</p>;
@@ -16,9 +19,16 @@ export default function Projects() {
     return <p>{error}</p>;
   }
 
+  function handleCloseForm() {
+    setShowAddForm(false);
+  }
+
   return (
     <div className={styles.page}>
-      
+      <button 
+        onClick={() => setShowAddForm(true)}>
+        New Project
+      </button>
       {projects.length === 0 ? <p>No projects found.</p> 
       :
       <div className={styles.project__container}>
@@ -28,7 +38,8 @@ export default function Projects() {
           </Link>
         ))}
       </div>}
-
+      {showAddForm && <AddNewProject onClose={handleCloseForm}/>}
     </div>
+    
   );
 }
