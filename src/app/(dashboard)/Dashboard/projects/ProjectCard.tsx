fa7b5a984e8d9ebd8ca74720d45e80f8bb0b8320
Heming Hanevik <heming.hanevik@gmail.com>
2026-03-26 23:59:46 +0100

    add remove methods from context

--- src/app/(dashboard)/Dashboard/projects/ProjectCard.tsx
+++ src/app/(dashboard)/Dashboard/projects/ProjectCard.tsx
@@ -1,11 +1,17 @@
 "use client"
 
-import { Project } from "@/context/ProjectContext"
+import { Project, useProjects } from "@/context/ProjectContext"
 import styles from './Projects.module.scss'
 
 export default function ProjectCard({id, name, description, createdAt}:Project) {
-    function handleDelete() {
-        console.log("project removed.")
+    const { removeProject, refreshProjects } = useProjects();
+
+    async function handleDelete (e: React.MouseEvent) {
+        e.stopPropagation();
+        e.preventDefault();
+        await removeProject(id);
+        await refreshProjects();
+        console.log("project removed.");
     }
 
     return (
@@ -13,7 +19,7 @@ export default function ProjectCard({id, name, description, createdAt}:Project) 
             <h3>{name}</h3>
             <p>{description}</p>
             <h6>{createdAt}</h6>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={(e) => handleDelete(e)}>Delete</button>
         </div>
     )
 }
